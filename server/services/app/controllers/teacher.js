ab6c11c77f87d29b862cb65a42addbd6f80bf82e
muhgifariakbar <muhgifariakbar@gmail.com>
2022-10-12 10:18:37 +0700

    add ordering to examBySubject

--- server/services/app/controllers/teacher.js
+++ server/services/app/controllers/teacher.js
@@ -420,7 +420,7 @@ class TeacherController {
     }
   }
 
- static async examScoreBySubject(req, res, next) {
+  static async examScoreBySubject(req, res, next) {
     try {
       // let { name } = req.query;
       let CourseId = req.user.CourseId;
@@ -468,10 +468,12 @@ class TeacherController {
                 where: {
                   CourseId,
                 },
+                order: [["id", "ASC"]],
               },
             ],
           },
         ],
+        order: [["fullName", "ASC"]],
       });
 
       return res.status(201).json(result);
